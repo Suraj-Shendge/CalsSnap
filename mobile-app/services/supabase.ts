f9fc60eaa353820792f50443e4fe0647bee7cc8d
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-03-16 16:11:36 +0530

    Update supabase.ts

--- mobile-app/services/supabase.ts
+++ mobile-app/services/supabase.ts
@@ -1,1 +1,8 @@
 
+import { createClient } from '@supabase/supabase-js';
+import Constants from 'expo-constants';
+
+const SUPABASE_URL = Constants?.manifest?.extra?.SUPABASE_URL ?? '';
+const SUPABASE_ANON_KEY = Constants?.manifest?.extra?.SUPABASE_ANON_KEY ?? '';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
