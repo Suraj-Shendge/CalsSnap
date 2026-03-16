867499a3df81bc22f5ecfa7843366d751ac3cee2
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-03-16 16:05:56 +0530

    Update dateHelpers.js

--- backend/utils/dateHelpers.js
+++ backend/utils/dateHelpers.js
@@ -1,1 +1,11 @@
 
+export function startOfDay(date) {
+  const d = new Date(date);
+  d.setHours(0, 0, 0, 0);
+  return d;
+}
+export function endOfDay(date) {
+  const d = new Date(date);
+  d.setHours(23, 59, 59, 999);
+  return d;
+}
