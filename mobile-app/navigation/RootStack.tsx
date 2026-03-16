9cb82b5e91b0e2a646ace96b41f30c478c677123
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-03-16 16:13:04 +0530

    Update RootStack.tsx

--- mobile-app/navigation/RootStack.tsx
+++ mobile-app/navigation/RootStack.tsx
@@ -1,1 +1,27 @@
 
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BottomTabs from './BottomTabs';
+import ResultScreen from '../screens/ResultScreen';
+import PaywallScreen from '../screens/PaywallScreen';
+import OfferScreen from '../screens/OfferScreen';
+
+export type RootStackParamList = {
+  Main: undefined;
+  Result: { scanResult: any };
+  Paywall: undefined;
+  Offer: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function RootNavigator() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Main" component={BottomTabs} />
+      <Stack.Screen name="Result" component={ResultScreen} />
+      <Stack.Screen name="Paywall" component={PaywallScreen} />
+      <Stack.Screen name="Offer" component={OfferScreen} />
+    </Stack.Navigator>
+  );
+}
