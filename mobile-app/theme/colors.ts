3eddf570cd45db50ad58bd4c79d70d654bcb4f6b
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-03-17 23:52:04 +0530

    Create colors.ts

---
+++ mobile-app/theme/colors.ts
@@ -0,0 +1,14 @@
+ const COLORS = {
+  primary: '#FF6A3D',
+
+  background: '#F8F9FB',
+  glass: 'rgba(255,255,255,0.75)',
+
+  text: '#111',
+  subText: '#666',
+
+  border: 'rgba(0,0,0,0.06)',
+  white: '#FFFFFF'
+};
+
+export default COLORS;
