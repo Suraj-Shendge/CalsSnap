5b33938fc6d23ba66bea0e173c838eb8a28e2772
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-03-17 17:46:59 +0530

    Update supabase.ts

--- mobile-app/services/supabase.ts
+++ mobile-app/services/supabase.ts
@@ -5,6 +5,9 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
+console.log("SUPABASE URL:", supabaseUrl);
+console.log("SUPABASE KEY:", supabaseAnonKey);
+
 export const supabase = createClient(
   supabaseUrl,
   supabaseAnonKey
