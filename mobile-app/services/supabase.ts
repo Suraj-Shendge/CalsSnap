d5de6810ea6bda3bb98dff12d219e1df0ef2351f
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-03-17 17:58:22 +0530

    Update supabase.ts

--- mobile-app/services/supabase.ts
+++ mobile-app/services/supabase.ts
@@ -1,9 +1,13 @@
-
 import { createClient } from '@supabase/supabase-js';
+import Constants from 'expo-constants';
 
+const supabaseUrl =
+  Constants.expoConfig?.extra?.SUPABASE_URL ??
+  process.env.EXPO_PUBLIC_SUPABASE_URL;
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseAnonKey =
+  Constants.expoConfig?.extra?.SUPABASE_ANON_KEY ??
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 console.log("SUPABASE URL:", supabaseUrl);
 console.log("SUPABASE KEY:", supabaseAnonKey);
