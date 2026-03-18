c7504726634d5801675e4ae3ac4c00a82a72f9f0
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-03-18 20:43:48 +0530

    Update RootStack.tsx

--- mobile-app/navigation/RootStack.tsx
+++ mobile-app/navigation/RootStack.tsx
@@ -1,12 +1,9 @@
-import HistoryScreen from '../screens/HistoryScreen';
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomTabs from './BottomTabs';
 import ResultScreen from '../screens/ResultScreen';
 import PaywallScreen from '../screens/PaywallScreen';
 import OfferScreen from '../screens/OfferScreen';
-import HomeScreen from '../screens/HomeScreen';
-
 
 export type RootStackParamList = {
   Main: undefined;
@@ -15,27 +12,23 @@ export type RootStackParamList = {
   Offer: undefined;
 };
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootStack() {
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
-
         animation: 'slide_from_right',
-
-        // 🔥 THIS is what makes it feel iOS-level smooth
         animationDuration: 250,
-
         gestureEnabled: true,
         fullScreenGestureEnabled: true
       }}
     >
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Main" component={BottomTabs} />
       <Stack.Screen name="Result" component={ResultScreen} />
-      <Stack.Screen name="History" component={HistoryScreen} />
       <Stack.Screen name="Paywall" component={PaywallScreen} />
+      <Stack.Screen name="Offer" component={OfferScreen} />
     </Stack.Navigator>
   );
 }
